43f7f5ccb4ead6418b533aa03f2d90aea527db98
Chase McCarthy <chase@code0100fun.com>
2014-11-19 10:39:42 -0800

    Initial commit

---
+++ test/parser_test.js
@@ -0,0 +1,54 @@
+var Parser = require('../lib/parser');
+
+describe('parser', function () {
+  it('parses tags', function () {
+    var ast = Parser.parse('%p');
+    expect(ast).to.deep.equal({ type: "Element", tag: "p" });
+  });
+
+  it('parses classes on elements', function () {
+    var ast = Parser.parse('%p.foo.bar');
+    expect(ast).to.deep.equal({
+      type: "Element",
+      tag: "p",
+      attributes: {
+        class : ['foo', 'bar']
+      }
+    });
+  });
+
+  it('parses id on element', function () {
+    var ast = Parser.parse('%p#baz');
+    expect(ast).to.deep.equal({
+      type: "Element",
+      tag: "p",
+      attributes: {
+        id : 'baz'
+      }
+    });
+  });
+
+  it('parses id and classes on element', function () {
+    var ast = Parser.parse('%p.foo#baz.bar');
+    expect(ast).to.deep.equal({
+      type: "Element",
+      tag: "p",
+      attributes: {
+        id : 'baz',
+        class : ['foo', 'bar']
+      }
+    });
+  });
+
+  it('defaults element to div if no tag is givin', function () {
+    var ast = Parser.parse('.foo#baz.bar');
+    expect(ast).to.deep.equal({
+      type: "Element",
+      tag: "div",
+      attributes: {
+        id : 'baz',
+        class : ['foo', 'bar']
+      }
+    });
+  });
+});
