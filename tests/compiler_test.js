294ad6429c41567989ad5bdd6bc286ae9f7a22d9
Chase McCarthy <chase@code0100fun.com>
2015-02-25 00:08:04 -0500

    Handle self closing tags

--- tests/compiler_test.js
+++ tests/compiler_test.js
@@ -7,6 +7,11 @@ describe('compiler', function () {
     expect(template).to.equal('<p id="paragraph" class="foo bar"></p>\n<a id="link" class="baz qux"></a>');
   });
 
+  it('self closing tags', function () {
+    var template = Compiler.compile('%meta\n%input\n%img');
+    expect(template).to.equal('<meta>\n<input>\n<img>');
+  });
+
   it('nested tags', function () {
     var template = Compiler.compile('%p\n  %a');
     expect(template).to.equal('<p>\n  <a></a>\n</p>');
